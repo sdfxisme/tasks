4338a557442e9383150cf4418d9ad76645f7efad
sdfxisme <sdfx@yandex.ru>
2020-01-09 04:51:29 +0300

    second commit

--- lesson4.py
+++ lesson4.py
@@ -2,7 +2,7 @@ import random
 list_100 = []
 list_20 = ['lesha', 'masha', 'sasha', 'dasha', 'misha', 'vasya', 'vanya', 'lena', 'nastya', 'mitya', 'dima',
                'kostya', 'andrey', 'anton', 'serge', 'marina', 'lyuda', 'kolya', 'yulya', 'pasha']
-# сначала сделал с randint, не не нашел куда засунуть def, решил не стирать
+# сначала сделал с randint, но не нашел куда засунуть def, и решил не стирать, потому что ответ тоже правильный
 for i in range(0,99):
     r = random.randint(0, 19)
     list_100.insert(i,list_20[r])
